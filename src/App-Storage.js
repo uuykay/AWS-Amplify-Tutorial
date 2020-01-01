ad94d6eff1f240d4fc4f50e0532c16d4a9c5123f
Will <william.y.kuang@gmail.com>
2020-01-01 17:15:22 +1100

    setup button change onclick

--- src/App-Storage.js
+++ src/App-Storage.js
@@ -13,10 +13,34 @@ class App extends React.Component {
     };
   }
 
+  handleChange = e => {
+    const file = e.target.files[0];
+    this.setState({
+      fileUrl: URL.createObjectURL(file),
+      file,
+      filename: file.name
+    });
+  };
+
+  saveFile = () => {
+    console.log("saveFile method");
+    Storage.put(this.state.filename, this.state.file)
+      .then(() => {
+        console.log("successfully saved file");
+        this.setState({ fileUrl: "", file: "", filename: "" });
+      })
+      .catch(err => {
+        console.log("error uploading file,", err);
+      });
+  };
+
   render() {
     return (
       <div className="App">
         <h1>Storage Demo</h1>
+        <input type="file" onChange={this.handleChange}></input>
+        <img alt="previewed file" src={this.state.fileUrl}></img>
+        <button onClick={this.saveFile}>Save File</button>
       </div>
     );
   }
